96078c12f8e31424181907d476a77e4f8dd0dc2c
ibonzanino <bonzaninoigor@gmail.com>
2022-03-14 17:55:39 -0300

    5th challenge

---
+++ creating_javascript_objects_one.js
@@ -0,0 +1,9 @@
+// Write a function that a string (a) as argument
+// Create an object that has a property with key 'key' and a value of a
+// Return the object
+function myFunction(a) {
+
+  return { key: a };
+}
+
+console.log(myFunction('a'));
