da318779e1ecd2fa6d4058c0ef2005cc2a6120e8
ibonzanino <bonzaninoigor@gmail.com>
2022-03-14 17:49:59 -0300

    4th challenge

---
+++ check_if_property_exists_in_object.js
@@ -0,0 +1,9 @@
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if a has a property with key b
+// Return false otherwise
+function myFunction(a, b) {
+
+  return b in a;
+}
+
+console.log(myFunction({x: 'a', y: 'b', z: 'c'}, 'z'));
