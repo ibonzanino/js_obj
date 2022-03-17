13aee4d4018ba0a9983c8f64f65bf64db5b7b2a2
ibonzanino <bonzaninoigor@gmail.com>
2022-03-16 21:34:12 -0300

    16th challenge

---
+++ add_property_to_each_object_in_array.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array of objects and a string as arguments
+// Add a property with key 'continent' and value equal to the string to each of the objects
+// Return the new array of objects
+// Tip: try not to mutate the original array
+
+function myFunction(arr, str) {
+  
+  return arr.map(obj => ({ ...obj, continent: str }));
+}
+
+console.log(myFunction([{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'));
